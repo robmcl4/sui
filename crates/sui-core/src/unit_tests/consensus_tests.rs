c6526d71b43cc0c40621c3617558506ae92b55af
Mark Logan <mark@mystenlabs.com>
2024-09-13 14:06:08 -0700

    wip

--- crates/sui-core/src/unit_tests/consensus_tests.rs
+++ crates/sui-core/src/unit_tests/consensus_tests.rs
@@ -222,6 +222,7 @@ pub fn make_consensus_adapter_for_test(
                                     vec![tx],
                                     &checkpoint_service,
                                     self.state.get_object_cache_reader().as_ref(),
+                                    self.state.get_transaction_cache_reader().as_ref(),
                                     &self.state.metrics,
                                     true,
                                 )
@@ -240,6 +241,7 @@ pub fn make_consensus_adapter_for_test(
                                 vec![tx],
                                 &checkpoint_service,
                                 self.state.get_object_cache_reader().as_ref(),
+                                self.state.get_transaction_cache_reader().as_ref(),
                                 &self.state.metrics,
                                 true,
                             )
